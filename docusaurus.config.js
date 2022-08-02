322cc739f3b66acdbadefc1b12ee11bab631e610
itxx00 <itxx00@gmail.com>
2022-08-02 15:48:00 +0800

    change proj name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'itxx00', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'notes', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/itxx00/docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/itxx00/notes/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/itxx00/docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/itxx00/notes/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -71,7 +71,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/itxx00/docs',
+            href: 'https://github.com/itxx00/notes',
             label: 'GitHub',
             position: 'right',
           },
@@ -85,7 +85,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/notes/intro',
               },
             ],
           },

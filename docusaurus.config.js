8f508f93de60522a7fe78ff16ab60e38932527a0
itxx00 <itxx00@gmail.com>
2022-08-03 17:14:00 +0800

    config update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/itxx00/notes/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/itxx00/notes/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/itxx00/notes/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/itxx00/notes/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
